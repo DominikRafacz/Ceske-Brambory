35bb2c21580ef46c5f99ae2b8991c960c0b17909
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-01-09 15:58:59 +0100

    fixed HBase setup

--- HBase/setup.sh
+++ HBase/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [[ $(echo "exists 'events'" | hbase shell | grep 'not exist') ]];
 then
-    echo "create 'events','hits'" | hbase shell;
+    echo "create 'events','h', 'r'" | hbase shell;
 fi
