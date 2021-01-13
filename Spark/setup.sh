cecce6dd4cfe7f00a78272dd3a138fbed74c3e3c
siemashko <k.siemaszko@student.mini.pw.edu.pl>
2021-01-13 22:56:58 +0100

    modified pyspark command

--- Spark/setup.sh
+++ Spark/setup.sh
@@ -2,4 +2,4 @@
 export JAVA_TOOL_OPTIONS="-Dhttps.protocols=TLSv1.2"
 pip install httplib2
 
-#pyspark --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0,org.apache.hbase:hbase-spark:2.0.0-alpha-1,com.databricks:spark-csv_2.10:1.5.0
+#pyspark --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0,org.apache.hbase:hbase-spark:2.0.0-alpha-1,com.databricks:spark-csv_2.10:1.5.0,org.apache.spark:spark-streaming-kafka-assembly_2.10:1.6.3
