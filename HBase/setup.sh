189c438c917b385058b81ab5ed713f8d10517617
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-01-13 22:43:31 +0100

    added hbase rest starting

--- HBase/setup.sh
+++ HBase/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 if [[ $(echo "exists 'events'" | hbase shell | grep 'not exist') ]];
 then
-    echo "create 'events','h', 'r'" | hbase shell;
+    echo "create 'events','h', 'r', 'c'" | hbase shell;
 fi
+
+/usr/hdp/current/hbase-master/bin/hbase-daemon.sh stop
+/usr/hdp/current/hbase-master/bin/hbase-daemon.sh start rest -p 8000
+
