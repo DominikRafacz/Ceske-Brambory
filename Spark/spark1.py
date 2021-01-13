5eed3df69a639ddd3a1071537e51d35b4ae79094
siemashko <k.siemaszko@student.mini.pw.edu.pl>
2021-01-13 21:43:29 +0100

    spark code clean up

--- Spark/spark1.py
+++ Spark/spark1.py
@@ -6,7 +6,7 @@ from httplib2 import Http
 import json
 
 
-def already_collected(event_id):
+def get_collected(event_id):
     response = Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/r:collected', 'GET')
     status = response[0]['status']
     return int(response[1]) if status == '200' else 0
@@ -16,13 +16,33 @@ def get_corrupted_count(event_id):
     status = response[0]['status']
     return int(response[1]) if status == '200' else 0
 
+def put_total_hits(event_id, total_hits):
+    return Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/r:total_hits', 'PUT',
+                           body=str(total_hits), headers={'content-type': 'application/octet-stream'})
+
+def put_corrupted_columns(event_id, hit_id, corrupted_columns):
+    return Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/c:' + str(hit_id), 'PUT',
+                           body=str(corrupted_columns), headers={'content-type': 'application/octet-stream'})
+
+def put_corrupted_count(event_id, corrupted_count):
+    return Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/c:count', 'PUT',
+                       body=str(corrupted_count), headers={'content-type': 'application/octet-stream'})
+
+def put_hit(event_id, hit_id, hit):
+    return Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/h:' + str(hit_id), 'PUT',
+                           body=str(hit), headers={'content-type': 'application/octet-stream'})
+
+def put_collected(event_id, collected):
+    return Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/r:collected', 'PUT',
+                       body=str(collected), headers={'content-type': 'application/octet-stream'})
 
 def send_completion_msg_to_kafka_topic(event_id):
-    pass
+    with open("/file.txt", "w") as f:
+                f.write("nie wysłałem " + str(event_id) + " bo jest corrupted")
 
 def verify_hit(hit):
     corrupted_columns = []
-    cols_to_verify = ["tx","ty","tz","tpx","tpy","tpz","weights"]
+    cols_to_verify = ["tx","ty","tz","tpx","tpy","tpz","weight"]
     for col in cols_to_verify:
         try: float(hit[col])
         except: corrupted_columns.append(col)
@@ -34,27 +54,23 @@ def put_in_hbase(events):
     events = events.collect()
     for event in events:
         event_id = event['event_id']
-        response = Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/r:total_hits', 'PUT',
-                           body=str(event["total_hits"]), headers={'content-type': 'application/octet-stream'})
+        total_hits = event['total_hits']
+        hits = event['data']
+        put_total_hits(event_id, total_hits)
         corrupted_count = 0
-        for hit in event['data']:
+        for hit in hits:
+            hit_id = hit['hit_id']
             corrupted_columns = verify_hit(hit)
             if len(corrupted_columns) != 0:
                 corrupted_count += 1
-                response = Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/c:' + str(hit['hit_id']), 'PUT',
-                           body=str(corrupted_columns), headers={'content-type': 'application/octet-stream'})
-            response = Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/h:' + str(hit['hit_id']), 'PUT',
-                           body=str(hit), headers={'content-type': 'application/octet-stream'})
+                put_corrupted_columns(event_id, hit_id, corrupted_columns)
+            put_hit(event_id, hit_id, hit)
         corrupted_count += get_corrupted_count(event_id)
-        response = Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/c:count', 'PUT',
-                       body=str(corrupted_count), headers={'content-type': 'application/octet-stream'})
-        ac = already_collected(event_id)
+        put_corrupted_count(event_id, corrupted_count)
+        ac = get_collected(event_id)
         new_ac = ac + len(event['data'])
-        response = Http().request('http://sandbox.hortonworks.com:8000/events/' + str(event_id) + '/r:collected', 'PUT',
-                       body=str(new_ac), headers={'content-type': 'application/octet-stream'})
-        if new_ac == event['total_hits']:
-            with open("/file.txt", "a") as f:
-                f.write("siemaaaa " + str(event_id))
+        put_collected(event_id, new_ac)
+        if new_ac == event['total_hits'] and get_corrupted_count(event_id) == 0 :
             send_completion_msg_to_kafka_topic(event_id)
 
 
