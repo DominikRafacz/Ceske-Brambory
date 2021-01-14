c41ca41286fc2c9eba39785f2a51a653fe68c99d
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-01-14 19:01:21 +0100

    fixed setup for kafka

--- Kafka/setup-kafka.sh
+++ Kafka/setup-kafka.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir /kafka-mock
+chmod 777 /kafka-mock
+
 /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partitions 1 --topic events
 /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partitions 1 --topic successes
 
