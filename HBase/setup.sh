82c44b6edc4a5a69984a0b257b29c8ab018d89da
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-01-17 13:26:38 +0100

    added missing command

--- HBase/setup.sh
+++ HBase/setup.sh
@@ -4,6 +4,6 @@ then
     echo "create 'events','h', 'r', 'c'" | hbase shell;
 fi
 
-/usr/hdp/current/hbase-master/bin/hbase-daemon.sh stop
+/usr/hdp/current/hbase-master/bin/hbase-daemon.sh stop rest
 /usr/hdp/current/hbase-master/bin/hbase-daemon.sh start rest -p 8000
 
